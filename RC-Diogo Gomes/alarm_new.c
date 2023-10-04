8bda39e6b057df507e398e5b2ddbdd766b8d1b0c
Diogo <95471762+Diogoafg7@users.noreply.github.com>
2023-10-04 15:58:22 +0100

    Add files via upload

--- RC-Diogo Gomes/alarm_new.c
+++ RC-Diogo Gomes/alarm_new.c
@@ -1,3 +1,4 @@
+/*
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -90,7 +91,7 @@ int main(int argc, char *argv[])
         exit(-1);
     }
 
-    printf("Nova estrutura de termios configurada\n");
+    printf("New termios structure set\n");
 
     unsigned char set[5] = {0x7E, 0x03, 0x03, 0x00, 0x7E};
     unsigned char ua[5];
@@ -106,7 +107,7 @@ int main(int argc, char *argv[])
         int bytes_wr = write(fd, set, sizeof(set));
         int bytes = read(fd, ua, sizeof(ua));
 
-        // Verifique se o quadro UA foi recebido corretamente
+        
         if (bytes == sizeof(ua) &&
             ua[0] == 0x7E &&
             ua[1] == 0x01 &&
@@ -115,15 +116,15 @@ int main(int argc, char *argv[])
             ua[4] == 0x7E)
         {
             printf("Quadro UA recebido corretamente. Desativando o alarme.\n");
-            alarm(0); // Desativar o alarme
+            alarm(0);
             break;     
         }
         else
         {
-            // Caso contrário, aguarde até a próxima tentativa
+            
             if (alarmEnabled == FALSE)
             {
-                alarm(3); // Defina o alarme para ser acionado em 3s
+                alarm(3); 
                 alarmEnabled = TRUE;
             }
         }
@@ -144,5 +145,158 @@ int main(int argc, char *argv[])
     printf("Programa encerrado\n");
 
     return 0;
+}*/
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE 256
+#define MAX_RETRANSMISSIONS 3
+#define TIMEOUT_SECONDS 3
+
+volatile int STOP = FALSE;
+
+int alarmEnabled = FALSE;
+int alarmCount = 0;
+int fd;
+
+void alarmHandler(int signal)
+{
+    alarmEnabled = FALSE;
+    alarmCount++;
+
+    printf("Alarm #%d\n", alarmCount);
+
+    if (alarmCount <= MAX_RETRANSMISSIONS)
+    {
+
+        unsigned char set[5] = {0x7E, 0x03, 0x03, 0x00, 0x7E};
+        set[3] = set[1] ^ set[2];
+        int bytesWritten = write(fd, set, sizeof(set));
+        printf("Retransmitting the SET frame...\n");
+    }
+    else
+    {
+        printf("Maximum number of retransmissions reached. Exiting the program.\n");
+        exit(1); // Exit the program after reaching the maximum number of retransmissions
+    }
 }
+
+int main(int argc, char *argv[])
+{
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0;
+    newtio.c_cc[VMIN] = 5;
+
+    tcflush(fd, TCIOFLUSH);
+
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    unsigned char set[5] = {0x7E, 0x03, 0x03, 0x00, 0x7E};
+    unsigned char ua[5];
+
+    set[3] = set[1] ^ set[2];
+
+    (void)signal(SIGALRM, alarmHandler);
+
+    int bytesWritten;
+
+    while (alarmCount < MAX_RETRANSMISSIONS)
+    {
+        int bytesWritten = write(fd, set, sizeof(set));
+        int bytesRead = read(fd, ua, sizeof(ua));
+
+        if (bytesRead == sizeof(ua) &&
+            ua[0] == 0x7E &&
+            ua[1] == 0x01 &&
+            ua[2] == 0x07 &&
+            ua[3] == (ua[1] ^ ua[2]) &&
+            ua[4] == 0x7E)
+        {
+            printf("UA frame received correctly. Disabling the alarm.\n");
+            alarm(0);
+            break;     
+        }
+
+        else
+        {
+            if (alarmEnabled == FALSE)
+            {
+                alarm(TIMEOUT_SECONDS); 
+                alarmEnabled = TRUE;
+            }
+        }
+    }
+
+    printf("%d bytes written\n", bytesWritten);
+
+    sleep(1);
+
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    printf("Program ended\n");
+
+    return 0;
+}
+
+
 
